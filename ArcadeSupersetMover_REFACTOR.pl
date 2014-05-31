8a4e70bad6cdc60eaed72cf17ec78e071f33ed33
Tony Woode <tonywoodecode@gmail.com>
2014-05-31 10:46:02 +0100

    loops implmented but not yet live

--- ArcadeSupersetMover_REFACTOR.pl
+++ ArcadeSupersetMover_REFACTOR.pl
@@ -15,32 +15,32 @@ sub OpChoice;
 sub SimChoice;
 sub OpenFileDirs;
 sub ParseQPFile;
+sub CloseFileDirs;
 
-$INPUTFILE = $ARGV[0];
+$inputfile = $ARGV[0];
 # INPUT YOUR DIRECTORIES HERE
 #--------------------------------------------------------------------------
-@INPUTDIR = ( #input dirs - no trailing \ please!!!!
+@inputdir = ( #input dirs - no trailing \ please!!!!
 	'F:\Arcade\TRANSIT\UNZIP\MAMESCREENIES',
 	'F:\Arcade\SCREENSHOTS\FBA_nonMAME_screenshots',
 	'F:\Sega Games\HazeMD\HazeMD\snap',
 	'F:\Arcade\SCREENSHOTS\Winkawaks_NONMAME_screenshots',
 );
-$OUTDIR = "F:\\Arcade\\TRANSIT"; #Output dir
+$outdir = "F:\\Arcade\\TRANSIT"; #Output dir
 #--------------------------------------------------------------------------
 
-
 print "\n\n" . "*"x30 . "\n\n Romdata Asset Matching Tool\n\n" . "*"x30 . "\n\n";
-$INPUTFILE eq ''? die "Quiting - You didn't pass me an input file" : print "Input file set to:\n $INPUTFILE\n\n"; 
-$OUTDIR eq ''? die "Quiting - You didn't program an output dir" : print "Output directory set to:\n $OUTDIR\n\n";
-if (scalar @INPUTDIR == 0) { die "Quiting - You didn't pass me any input directories"; }
-else { foreach $index ( 0 .. $#INPUTDIR ) { print "Input directory $index set to $INPUTDIR[$index]\n"; } }
+$inputfile eq ''? die "Quiting - You didn't pass me an input file" : print "Input file set to:\n $inputfile\n\n"; 
+$outdir eq ''? die "Quiting - You didn't program an output dir" : print "Output directory set to:\n $outdir\n\n";
+if (scalar @inputdir == 0) { die "Quiting - You didn't pass me any input directories"; }
+else { foreach $index ( 0 .. $#inputdir ) { print "Input directory $index set to $inputdir[$index]\n"; } }
 
 #Ask what we're doing
-OpChoice;
-SimChoice;
-OpenFileDirs;
-ParseQPFile;
-
+OpChoice();
+SimChoice();
+OpenFileDirs();
+ParseQPFile();
+CloseFileDirs();
 
 #Subroutines
 #------------------------------------------------------------------------
@@ -61,7 +61,7 @@ else { die "\nNo, that's not sensible. Try again with a choice that's in the men
 );	
 
 $fileType = $fileTypes{$opType};
-$OUTPUTDIR = "$OUTDIR\\$opType"; #SET THE OUTPUT DIRECTORY BASED ON OPTYPE NAME
+$outputdir = "$outdir\\$opType"; #SET THE OUTPUT DIRECTORY BASED ON OPTYPE NAME
 print "So I'm going to look for:\t$fileType\n\n";
 }
 
@@ -81,17 +81,16 @@ sub SimChoice { #Give user choice of behaviour
 
 #------------------------------------------------------------------------
 sub OpenFileDirs {
-if ($copy) { make_path $OUTPUTDIR;}
-if ($copy) { make_path "$OUTPUTDIR\\Parentchild";} # make this dir for image types in case we need it later
-$HAVEFILE = "$OUTDIR\\Have$opType.txt";
-$PARENTCHILDFILE = "$OUTDIR\\ParentChild$opType.txt";
-$MISSFILE = "$OUTDIR\\Miss$opType.txt";
+if ($copy) { make_path $outputdir;}
+if ($copy) { make_path "$outputdir\\Parentchild";} # make this dir for image types in case we need it later
+$havefile = "$outdir\\Have$opType.txt";
+$parentchildfile = "$outdir\\ParentChild$opType.txt";
+$missfile = "$outdir\\Miss$opType.txt";
 
-
-open(QPDATFILE, $INPUTFILE) or die "Cannot open Quickplay dat file\n";
-open(HAVEFILE, ">$HAVEFILE");
-open(PARENTCHILDFILE, ">$PARENTCHILDFILE");
-open(MISSFILE, ">$MISSFILE");
+open(QPDATFILE, $inputfile) or die "Cannot open Quickplay dat file\n";
+open(HAVEFILE, ">$havefile");
+open(PARENTCHILDFILE, ">$parentchildfile");
+open(MISSFILE, ">$missfile");
 }
 
 #------------------------------------------------------------------------
@@ -103,25 +102,38 @@ sub ParseQPFile {
 	$qp_pattern = "([^$QPS]*)$QPS"; #...so a Quickplay romdata entry consists of this pattern... 
 	$qp_line = "$qp_pattern"x19; #...thus a line of Quickplay romdata consits of that entry repeated 19 times
 	
-	while ($line=<QPDATFILE>)
-	{
+	print "\nScanning...\n";
+	while ($line=<QPDATFILE>) {
 		chomp $line;
 		if ($line =~ /^$qp_line/)
 		{
 			$mameName = $2; #mamename
 			$mameParent = $3; #parent romname
 			
-			$OUTPUTDIR = "$OUTDIR\\$opType"; #previous image may have changed the output dir to \\parentchild
+			$outputdir = "$outdir\\$opType"; #previous image may have changed the output dir to \\parentchild
 			
-			$path1 = "$INPUTDIR[0]\\$mameName$fileType";			
-			$path2 = "$INPUTDIR[1]\\$mameName$fileType";
-			$path3 = "$INPUTDIR[2]\\$mameName$fileType";
-			$path4 = "$INPUTDIR[3]\\$mameName$fileType";
+			#foreach my $path ( 0 .. $#inputdir ) { push (@search_path, "$inputdir[$path]\\$mameName$fileType"); }
+			#foreach my $path ( 0 .. $#inputdir ) { push (@parent_search_path, "$inputdir[$path]\\$mameParent$fileType"); } 
+			
+			#that should get rid of this lot
+			$path1 = "$inputdir[0]\\$mameName$fileType";			
+			$path2 = "$inputdir[1]\\$mameName$fileType";
+			$path3 = "$inputdir[2]\\$mameName$fileType";
+			$path4 = "$inputdir[3]\\$mameName$fileType";
 			#if its not held as the mamename, we try the parent name. this may be blank....
-			$path5 = "$INPUTDIR[0]\\$mameParent$fileType";
-			$path6 = "$INPUTDIR[1]\\$mameParent$fileType";
-			$path7 = "$INPUTDIR[2]\\$mameParent$fileType";
-			$path8 = "$INPUTDIR[3]\\$mameParent$fileType";
+			$path5 = "$inputdir[0]\\$mameParent$fileType";
+			$path6 = "$inputdir[1]\\$mameParent$fileType";
+			$path7 = "$inputdir[2]\\$mameParent$fileType";
+			$path8 = "$inputdir[3]\\$mameParent$fileType";
+			
+			
+			
+			#foreach my $path ( 0 .. $#search_path  ) {
+			#	if ($search_path ne '' && -e $search_path) {$there ++; $foundPath = $search_path;  printf HAVEFILE  ("%-15s %-15s %-25s %-15s", "$mameName", "Found", "Child is in $path"," = $search_path\n"); 
+			#	if -e $search_path ( last );
+			#	} 
+			
+			
 			
 			if 	  ($path1 ne '' && -e $path1) {$there ++; $foundPath = $path1;  printf HAVEFILE  ("%-15s %-15s %-25s %-15s", "$mameName", "Found", "Child is in path1"," = $path1\n"); } 
 			elsif ($path2 ne '' && -e $path2) {$there ++; $foundPath = $path2;	printf HAVEFILE  ("%-15s %-15s %-25s %-15s", "$mameName", "Found", "Child is in path2"," = $path2\n"); } 
@@ -131,15 +143,15 @@ sub ParseQPFile {
 			# but here's the thing: do we copy the parent as the child rom's name? Theoretically we don't need, but then we WILL come across
 			# instances where the MAME parent gets renamed in a later version. We certainly don't want to copy parent ROMS with child names, that would
 			# be a disaster....lets put these in a subdir for now "parentchild"
-			elsif ($opType ne 'Roms' && $path5 ne '' && -e $path5) {$there ++; $foundPath = $path5; $OUTPUTDIR = "$OUTPUTDIR\\parentchild"; printf PARENTCHILDFILE ("%-15s %-15s %-25s %-15s", "$mameName", "Missing", "Parent is in path5"," = $path5\n"); }
-			elsif ($opType ne 'Roms' && $path6 ne '' && -e $path6) {$there ++; $foundPath = $path6; $OUTPUTDIR = "$OUTPUTDIR\\parentchild"; printf PARENTCHILDFILE ("%-15s %-15s %-25s %-15s", "$mameName", "Missing", "Parent is in path6"," = $path6\n"); }
-			elsif ($opType ne 'Roms' && $path7 ne '' && -e $path7) {$there ++; $foundPath = $path7; $OUTPUTDIR = "$OUTPUTDIR\\parentchild"; printf PARENTCHILDFILE ("%-15s %-15s %-25s %-15s", "$mameName", "Missing", "Parent is in path7"," = $path7\n"); }
-			elsif ($opType ne 'Roms' && $path8 ne '' && -e $path8) {$there ++; $foundPath = $path8; $OUTPUTDIR = "$OUTPUTDIR\\parentchild"; printf PARENTCHILDFILE ("%-15s %-15s %-25s %-15s", "$mameName", "Missing", "Parent is in path8"," = $path8\n"); }
+			elsif ($opType ne 'Roms' && $path5 ne '' && -e $path5) {$there ++; $foundPath = $path5; $outputdir = "$outputdir\\parentchild"; printf PARENTCHILDFILE ("%-15s %-15s %-25s %-15s", "$mameName", "Missing", "Parent is in path5"," = $path5\n"); }
+			elsif ($opType ne 'Roms' && $path6 ne '' && -e $path6) {$there ++; $foundPath = $path6; $outputdir = "$outputdir\\parentchild"; printf PARENTCHILDFILE ("%-15s %-15s %-25s %-15s", "$mameName", "Missing", "Parent is in path6"," = $path6\n"); }
+			elsif ($opType ne 'Roms' && $path7 ne '' && -e $path7) {$there ++; $foundPath = $path7; $outputdir = "$outputdir\\parentchild"; printf PARENTCHILDFILE ("%-15s %-15s %-25s %-15s", "$mameName", "Missing", "Parent is in path7"," = $path7\n"); }
+			elsif ($opType ne 'Roms' && $path8 ne '' && -e $path8) {$there ++; $foundPath = $path8; $outputdir = "$outputdir\\parentchild"; printf PARENTCHILDFILE ("%-15s %-15s %-25s %-15s", "$mameName", "Missing", "Parent is in path8"," = $path8\n"); }
 			
 			else { $notThere ++; print "Can't find\t=\t$mameName\n"; print MISSFILE "Can't find\t=\t$mameName\n"}
 			
 			#now do it - we hopefully never copy a parent rom as child name....
-			if ($copy) { print HAVEFILE "Copying $foundPath to $OUTPUTDIR\\$mameName$fileType\n"; copy $foundPath, "$OUTPUTDIR\\$mameName$fileType"; }
+			if ($copy) { print HAVEFILE "Copying $foundPath to $outputdir\\$mameName$fileType\n"; copy $foundPath, "$outputdir\\$mameName$fileType"; }
 		}
 		
 	}
@@ -147,7 +159,10 @@ sub ParseQPFile {
 	printf "%-50s %10u", "\nnumber of mamenames present as child or parent:\t", (defined $there ? "$there" : "0");
 	#defined $notThere ? print "\nnumber of files not present = $notThere" : print "\nNo files not found" ;
 	printf "%-46s %10u", "\nNumber of mamenames not found:\t", (defined $notThere ? "$notThere" : "0");
+}
 
+sub CloseFileDirs {
+	close(QPDATFILE);
 	close(HAVEFILE);
 	close(PARENTCHILD);
 	close(MISSFILE);
